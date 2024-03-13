933e132fd0396746b3dc444b439a277a38585e08
Drew Brown <drewkyle01@gmail.com>
2024-03-13 11:50:58 -0400

    Create teams dropdown

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,17 +1,17 @@
 "use client"
-// import { Button } from '@/components/ui/button'
+import { Button } from '@/components/ui/button'
 import { api } from '@/convex/_generated/api'
 import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
 import { useConvex, useMutation, useQuery } from 'convex/react'
 import React, { useEffect } from 'react'
-// import Header from './_components/Header'
-// import FileList from './_components/FileList'
-// import AdBanner from './../../_components/AdBanner'
+import Header from './_components/Header'
+import FileList from './_components/FileList'
+import AdBanner from './../../_components/AdBanner'
 function Dashboard() {
 
   const convex=useConvex();
   const {user}:any=useKindeBrowserClient();
-  //const getUser=useQuery(api.user.getUser,{email:user?.email});
+  const getUser=useQuery(api.user.getUser,{email:user?.email});
 
   const createUser=useMutation(api.user.createUser);
   useEffect(()=>{
@@ -38,6 +38,7 @@ function Dashboard() {
   }
   return (
     <div className='p-8'>
+      <h1>Hello</h1>
       <Header/>
 
       <FileList/>
