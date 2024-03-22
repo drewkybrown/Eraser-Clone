25cd704b5f9268363a7c662473a090bc1ace7986
Drew Brown <drewkyle01@gmail.com>
2024-03-22 11:23:27 -0400

    Prepare for deployment

--- app/(routes)/dashboard/_components/SideNavTopSection.tsx
+++ app/(routes)/dashboard/_components/SideNavTopSection.tsx
@@ -1,139 +1,99 @@
-import { ChevronDown, LayoutGrid, LogOut, Settings, Users } from 'lucide-react'
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover"
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
-import { Separator } from '@/components/ui/separator'
-import { useConvex } from 'convex/react'
-import { api } from '@/convex/_generated/api'
-import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
-
-export interface TEAM{
-    createdBy:String,
-    teamName:String,
-    _id:String
-}
-function SideNavTopSection({user,setActiveTeamInfo}:any) {
-    const menu=[
-        {
-            id:1,
-            name:'Create Team',
-            path:'/teams/create',
-            icon:Users
-        },
-        {
-            id:2,
-            name:'Settings',
-            path:'',
-            icon:Settings
-        }
-    ];
-    const router=useRouter();
-    const convex=useConvex();
-    const [activeTeam,setActiveTeam]=useState<TEAM>();
-    const [teamList,setTeamList]=useState<TEAM[]>();
-    useEffect(()=>{
-        user&&getTeamList();
-    },[user])
-
-    useEffect(()=>{
-        activeTeam?setActiveTeamInfo(activeTeam):null
-    },[activeTeam])
-    const getTeamList=async()=>{
-        const result=await convex.query(api.teams.getTeam,{email:user?.email})
-        console.log("TeamList",result);
-        setTeamList(result);
-        setActiveTeam(result[0]);
+import { Archive, Flag, Github } from 'lucide-react'
+import React, { useState } from 'react'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from '@/components/ui/input'
+import Constant from '@/app/_constant/Constant'
+import PricingDialog from './PricingDialog'
+function SideNavBottomSection({onFileCreate,totalFiles}:any) {
+  const menuList=[
+    {
+      id:1,
+      name:'Getting Started',
+      icon:Flag,
+      path:''
+    },
+    {
+      id:2,
+      name:'Github',
+      icon:Github,
+      path:''
+    },
+    {
+      id:3,
+      name:'Archive',
+      icon:Archive,
+      path:''
     }
+  ]
+  const [fileInput,setFileInput]=useState('');
+  return (
+    <div>
+      {menuList.map((menu,index)=>(
+        <h2 key={index} className='flex gap-2 p-1 px-2 text-[14px] 
+        hover:bg-gray-100 rounded-md cursor-pointer'>
+          <menu.icon className='h-5 w-5'/>
+          {menu.name}</h2>
+      ))}
 
-    const onMenuClick=(item:any)=>{
-        if(item.path)
-        {
-            router.push(item.path);
-        }
-    }
-    return (
-        <div>
-        <Popover>
-            <PopoverTrigger>
-                <div className='flex items-center gap-3
-      hover:bg-slate-200 p-3 rounded-lg
-      cursor-pointer
-      '>
-                    <Image src='/logo-1.png' alt='logo'
-                        width={40}
-                        height={40} />
-                    <h2 className='flex gap-2 
-                    items-center
-      font-bold text-[17px]
-      '>{activeTeam?.teamName}
-                        <ChevronDown />
-                    </h2>
+      {/* Add New File Button  */}
+      <Dialog>
+  <DialogTrigger className='w-full' asChild>
+  <Button className='w-full bg-blue-600 
+      hover:bg-blue-700 justify-start mt-3'>New File</Button>
+  </DialogTrigger>
+  {totalFiles<Constant.MAX_FREE_FILE? 
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Create New File</DialogTitle>
+      <DialogDescription>
+          <Input placeholder='Enter File Name' 
+          className='mt-3'
+          onChange={(e)=>setFileInput(e.target.value)}
+          />
+      </DialogDescription>
+    </DialogHeader>
+    <DialogFooter className="">
+          <DialogClose asChild>
+            <Button type="button" 
+            className='bg-blue-600
+            hover:bg-blue-700'
+            disabled={!(fileInput&&fileInput.length>3)}
+            onClick={()=>onFileCreate(fileInput)}
+            >
+              Create
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+  </DialogContent>:
+  <PricingDialog/>}
+</Dialog>
 
-                </div>
-            </PopoverTrigger>
-            <PopoverContent className='ml-7 p-4'>
-               {/* Team Section  */}
-                <div>
-                    {teamList?.map((team,index)=>(
-                        <h2 key={index}
-                        className={`p-2 hover:bg-blue-500
-                         hover:text-white
-                         rounded-lg mb-1 cursor-pointer
-                         ${activeTeam?._id==team._id&&'bg-blue-500 text-white'}`}
-                         onClick={()=>setActiveTeam(team)}
-                        >{team.teamName}</h2>
-                    ))}
-                    
-                </div>
-                <Separator className='mt-2 bg-slate-100'/>
-                {/* Option Section  */}
-                <div>
-                    {menu.map((item,index)=>(
-                        <h2 key={index} className='flex gap-2 items-center
-                        p-2 hover:bg-gray-100 rounded-lg cursor-pointer text-sm'
-                        onClick={()=>onMenuClick(item)}>
-                            <item.icon className='h-4 w-4'/>
-                            {item.name}</h2>
-                    ))}
-                    <LogoutLink>
-                    <h2 className='flex gap-2 items-center
-                        p-2 hover:bg-gray-100 rounded-lg cursor-pointer text-sm'>
-                            <LogOut className='h-4 w-4'/>
-                            Logout</h2>
-                        </LogoutLink>
-                </div>
-                <Separator className='mt-2 bg-slate-100'/>
-                {/* User Info Section  */}
-               {user&& <div className='mt-2 flex gap-2 items-center'>
-                    <Image src={user?.picture} alt='user'
-                    width={30}
-                    height={30}
-                    className='rounded-full'
-                    />
-                    <div>
-                        <h2 className='text-[14px] font-bold'>{user?.given_name} {user?.family_name}</h2>
-                        <h2 className='text-[12px] text-gray-500'>{user?.email}</h2>
+     
+      
+      {/* Progress Bar  */}
+      <div className='h-4 w-full bg-gray-200 rounded-full mt-5'>
+          <div className={`h-4  bg-blue-600 rounded-full`}
+          style={{ width: `${(totalFiles/5)*100}%` }}
+         >
+          </div>
+      </div>
 
-                    </div>
-                </div>}
-            </PopoverContent>
-        </Popover>
+      <h2 className='text-[12px] mt-3'>
+        <strong>{totalFiles}</strong> out of <strong>{Constant.MAX_FREE_FILE}</strong> files used</h2>
+      <h2 className='text-[12px] mt-1'>Upgrade your plan for unlimited access.</h2>  
 
-        {/* All File Button  */}
-        <Button variant='outline'
-         className='w-full justify-start
-          gap-2 font-bold mt-8 bg-gray-100'>
-            <LayoutGrid className='h-5 w-5'/>
-            All Files</Button>
-        </div>
-
-    )
+     </div>
+  )
 }
 
-export default SideNavTopSection
+export default SideNavBottomSection
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "./ConvexClientProvider";
+import { Toaster } from "@/components/ui/sonner";
+import Head from "next/head";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +20,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       <Head>
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
-      </body>
+        <ConvexClientProvider>
+         {children}
+         <Toaster />
+        </ConvexClientProvider>
+        </body>
     </html>
   );
 }
